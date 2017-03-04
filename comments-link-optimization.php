649b0042a7f2775e1734e7d2a9786d0341e4cdd4
zheyu <lite3@qq.com>
2017-03-05 01:08:21 +0800

    refactor and remove robots.txt

--- comments-link-optimization.php
+++ comments-link-optimization.php
@@ -5,116 +5,127 @@
 /*
 Plugin Name: Comments Link Optimization
 Plugin URI: http://www.litefeel.com/comments-link-optimization/
-Description: Comments Link Optimization waht prevent all search engine crawl your comments link. 
-Version: 1.8.4.1
+Description: Comments Link Optimization what prevent all search engine crawl your comments link. 
+Version: 1.9
 Author: lite3
 Author URI: http://www.litefeel.com/
-
-Copyright (c) 2011
-Released under the GPL license
-http://www.gnu.org/licenses/gpl.txt
+License: GPLv2 or later
+Text Domain: comments-link-optimization
 */
 
-//comments link redirect
-add_filter('comment_text', 'add_redirect_comment_text', 99);
-function add_redirect_comment_text($text = ''){
-	$home = get_option('home');
-	$len = strlen($home);
-	$pos = stripos($text, 'href=');
-	while($pos !== FALSE){
-		if('"' == $text[$pos+5] || '\'' == $text[$pos+5]){
-			// for reply
-			if('#' == $text[$pos+6]){
-				$pos += 7;
-			}
-			// for self link
-			else if(substr($text, $pos+6, $len) == $home){
-				$pos += $len + 7;
-			}
-			// for other link
-			else {
-				$text = substr($text, 0, $pos+6) . $home . '/?r=' . substr($text, $pos + 6);
-				$pos += $len + 7;
-			}
-		}else{
-			$pos += 7;
-		}
-		$pos = stripos($text, 'href=', $pos);
-	}
-	return $text;
+
+// Make sure we don't expose any info if called directly
+if ( !function_exists( 'add_action' ) ) {
+	echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
+	exit;
 }
 
-add_filter('get_comment_author_url', 'add_redirect_get_comment_author_url', 99);
-function add_redirect_get_comment_author_url($url){
-	$home = get_option('home');
-	if($url && stripos($url, $home) !== 0 && !is_admin()){
-		$url = $home . '/?r=' . $url;
+
+class CommentsLinkOptimization
+{
+
+function __construct() {
+	add_action('init', 'init');
+	add_filter('comment_text', array($this, 'modifyCommentText'), 99);
+	add_filter('get_comment_author_url', array($this, 'modifyCommentAuthorUrl'), 99);
+}
+
+function init() {
+	load_plugin_textdomain( 'comments-link-optimization' );
+	$this->checkRedirect();
+}
+
+function modifyCommentAuthorUrl($url){
+	// global $wp;
+	// $current_url = add_query_arg( $wp->query_string, '', home_url( $wp->request ) );
+
+	if (!is_admin()) {
+		$home = home_url();
+		if(!empty($url) && stripos($url, $home) !== 0){
+			$url = "$home/?r=$url";
+		}
 	}
 	return $url;
 }
 
-add_action('init', 'redirect_comment_link');
-function redirect_comment_link(){
-	$redirect = isset($_GET['r']) ? $_GET['r'] : false;
+function modifyCommentText($text){
+	if (!is_admin()) {
+		$home = home_url();
+		$newtext = preg_replace_callback(
+			'/<a [^>]*?href=[\'"]\s*([^\s#]\S+)\s*[\'"][^>]*?>/',
+			function($match) use ($home) {
+				$url = $matchs[1];
+				if (stripos($url, $home) !== 0) {
+					return "$home/?=$url";
+				}
+				return $url;
+			},
+			$text
+		);
+	}
+	return $text;
+}
+
+// 
+function checkRedirect() {
+	$redirect = isset($_GET['r']) ? $_GET['r'] : FALSE;
 	if($redirect){
-		if(empty($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'],get_option('home')) !== false){
+		$home = home_url();
+		if(empty($_SERVER['HTTP_REFERER']) || strpos($_SERVER['HTTP_REFERER'], $home) !== FALSE){
 			//header("Location: $redirect");
-			output_redirect($redirect);
+			$this->printHTML($redirect);
 		}else {
-			header('Location: ' . get_option('home'));
+			header("Location: $home");
 		}
 		exit;
 	}
-	
-	check_robots();
 }
 
-function output_redirect($url)
+function printHTML($url)
 {
-echo '<html><head><meta http-equiv="content-type" content="text/html; charset=UTF-8"><title>重新转向通知</title>
-<meta name="robots" content="noindex, nofollow"><style><!--
-body,td,div,.p,a{font-family:arial,sans-serif}
-div,td{color:#000}
-.f{color:#6f6f6f}
-a:link{color:#00c}
-a:visited{color:#551a8b}
-a:active{color:red}
-div.a{border-top:1px solid #bbb;border-bottom:1px solid #bbb;background:#f2f2f2;margin-top:1em;width:100%}
-div.b{padding:0.5em 0;margin-left:10px}
-div.c{margin-top:35px;margin-left:35px}
---></style>
-<script>function go_back() {window.history.go(-1);return false;}
-setTimeout(function(){ window.location.href="'.$url.'";},3000);
-</script></head>';
-echo '<body topmargin=3 bgcolor=#ffffff marginheight=3><div class=a><div class=b><font size=+1><b>重新转向通知</b></font></div></div><div class=c>&nbsp;您所在的网页3秒后自动您带往 <a href="' . $url . '">' .$url . '</a> 。<br><br>&nbsp;如果您不想造访该网页，您可以<a href="#" onclick="return go_back();">返回前一页</a>。<br><br><br></div></body></html>';
+	$title = __('Redirecting', 'comments-link-optimization');
+	$jumpContent = sprintf(
+		/* translators: %s: Html tag <a> */
+		__('The page will jump to %s after 3 seconds.', 'comments-link-optimization'),
+		"<a href=\"$url\">$url</a>"
+	);
+	$backContent = sprintf(
+		/* translators: 1: The begin of html tag <a> 2: The end of html tag <a> */
+		__('If you do not want to visit the page, you can %1%s return to the previous page %2%s .'),
+		'<a href="#" onclick="return goback();">',
+		'</a>'
+	);
+	
+echo <<<EOT
+<html>
+<head>
+	<meta name="robots" content="noindex, nofollow">
+	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+	<title>$title</title>
+	<style type="text/css">
+		body,td,div,.p,a{font-family:arial,sans-serif}
+		div,td{color:#000}
+		.f{color:#6f6f6f}
+		a:link{color:#00c}
+		a:visited{color:#551a8b}
+		a:active{color:red}
+		div.a{border-top:1px solid #bbb;border-bottom:1px solid #bbb;background:#f2f2f2;margin-top:1em;width:100%}
+		div.b{padding:0.5em 0;margin-left:10px}
+		div.c{margin-top:35px;margin-left:35px}
+	</style>
+	<script type="text/javascript">
+		function goback() {window.history.go(-1);return false;}
+		setTimeout(function(){window.location.href="$url";},3000);
+	</script>
+</head>
+<body topmargin=3 bgcolor=#ffffff marginheight=3>
+<div class=a><div class=b><font size=+1><b>$title</b></font></div></div><div class=c>&nbsp;$jumpContent
+<br><br>&nbsp;$backContent<br><br><br></div>
+</body>
+</html>
+EOT;
 }
 
-function check_robots(){
-	$path = parse_url(get_option('home') . '/');
-	$root = str_replace( $_SERVER["PHP_SELF"], '', $_SERVER["SCRIPT_FILENAME"] );
-	$robots_path = $root . '/robots.txt';
-	
-	$m_time = -1;
-	if(file_exists($robots_path)){
-		$m_time = filemtime($robots_path);
-	}
-	
-	$old_time = get_option('robots_modify_time');
-	if(FALSE == $old_time || $old_time != $m_time || $m_time <= 0){
-		$disallow = 'Disallow: ' . $path['path'] . '?r=*';
-		$str = -1 == $m_time ? '' : file_get_contents($robots_path);
-		if(FALSE === strpos($str, $disallow)){
-			$str .= "\n" . $disallow;
-			file_put_contents($robots_path, $str);
-			$m_time = filemtime($robots_path);
-		}
-		
-		if(FALSE == $old_time){
-			add_option('robots_modify_time', $m_time);
-		}else{
-			update_option('robots_modify_time', $m_time);
-		}
-	}
 }
 
-?>
+$commentsLinkOptimization = new CommentsLinkOptimization();
